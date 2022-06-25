9f0e615e98cd392a702ad43370ab444020903648
ggssh <2821764200@qq.com>
2022-06-25 21:38:10 +0800

    test

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,16 +3,37 @@
     windows_subsystem = "windows"
 )]
 
+use std::collections::HashMap;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}", name)
+    format!("Mihayou, {}", name)
+}
+
+#[tauri::command]
+fn get_vec(len: u32) -> Vec<i32> {
+    // dbg!(len);
+    let mut v = vec![];
+    for i in 0..len {
+        v.push(i as i32);
+    }
+    v
+}
+
+#[tauri::command]
+fn get_map(len: u32) -> HashMap<u32, String> {
+    let mut m = HashMap::new();
+    for i in 0..len {
+        m.insert(i, (i + 1).to_string());
+    }
+    m
 }
 
 fn main() {
     let context = tauri::generate_context!();
     tauri::Builder::default()
         .menu(tauri::Menu::os_default(&context.package_info().name))
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, get_vec, get_map])
         .run(context)
         .expect("error while running tauri application");
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,13 +16,17 @@
 <script>
   import { invoke } from "@tauri-apps/api"
   import { ref } from 'vue'
+  import { open } from '@tauri-apps/api/dialog';
+  // import { appDir } from '@tauri-apps/api/path';
+  import { readTextFile, BaseDirectory } from '@tauri-apps/api/fs';
 
   export default {
     name: 'HelloWorld',
     data() {
       return {
         count: 0,
-        msg: 'Hello Vue 3 + Vite'
+        msg: 'Hello Vue 3 + Vite',
+        file_content: '',
       }
     },
     setup() {
@@ -33,8 +37,47 @@
     },
     methods: {
       my_invoke() {
+        let msgs = [
+          'Hello, World!',
+          'Hello, Vue!',
+          'Hello, Vue 3 + Vite!'
+        ]
         invoke('greet', { name: 'World' })
-          .then((response) => this.msg = response)
+          .then((response) => this.msg = msgs[this.count % 3] + response)
+      },
+      get_vec() {
+        invoke('get_vec', { len: 5 })
+          .then((response) => console.log(response))
+      },
+      get_map() {
+        invoke('get_map', { len: 5 })
+          .then((response) => console.log(response))
+      },
+      // 要用异步函数
+      async open_file() {
+        const selected = await open({
+          multiple: false,
+          defaultPath: 'F:\\main\\projects',
+          filters: [
+            {
+              name: 'Text',
+              extensions: ['txt']
+            }]
+        }).catch(err => console.log(err));
+        if (Array.isArray(selected)) {
+          // user selected multiple files
+          console.log(selected);
+          const contents = await readTextFile(selected[0]).catch(err => console.log(err));
+          this.file_content = contents;
+        } else if (selected === null) {
+          // user cancelled the selection
+          console.log('cancelled');
+        } else {
+          // user selected a single file
+          console.log(selected);
+          const contents = await readTextFile(selected[0]).catch(err => console.log(err));
+          this.file_content = contents;
+        }
       }
     }
   }
@@ -43,27 +86,33 @@
 <template>
   <h1>{{ msg }}</h1>
 
-  <p>
+  <!-- <p>
     Recommended IDE setup:
     <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
     +
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
+  </p> -->
 
-  <p>
+  <!-- <p>
     <a href="https://vitejs.dev/guide/features.html" target="_blank">
       Vite Documentation
     </a>
     |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
+  </p> -->
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <button type="button" @click="my_invoke">invoke</button>
-  <p>
+  <div id="d1">
+    <button type="button" @click="count++">count is: {{ count }}</button>
+    <button type="button" @click="my_invoke">invoke me</button>
+    <button type="button" @click="get_vec">get_vec</button>
+    <button type="button" @click="get_map">get_map</button>
+    <button type="button" @click="open_file">open_file</button>
+  </div>
+  <p>{{file_content}}</p>
+  <!-- <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  </p> -->
 </template>
 
 <style scoped>
